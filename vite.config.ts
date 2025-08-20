c7b801c7265371fc48df9e75e6b3cf43d855168f
viccent55 <viccent55@gmail.com>
2025-08-20 17:00:09 +0700

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -53,6 +53,7 @@ const viteConfig = defineConfig((mode: ConfigEnv) => {
 
         workbox: {
           globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+          navigateFallbackDenylist: [/\.m3u8$/, /\.ts$/],
         },
         devOptions: {
           enabled: process.env.NODE_ENV === "development",
