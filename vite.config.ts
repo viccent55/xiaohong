4079576782f0b97cc2ead650fdc2c8363b0c32c1
viccent55 <viccent55@gmail.com>
2025-09-01 11:22:30 +0700

    update env

--- vite.config.ts
+++ vite.config.ts
@@ -72,7 +72,7 @@ const viteConfig = defineConfig((mode) => {
       proxy: {
         "/apiv1": {
           target:
-            mode.mode === "server"
+            process.env.NODE_ENV !== "development"
               ? env.VITE_API_URL_PROD
               : env.VITE_API_URL_LOC,
           changeOrigin: true,
